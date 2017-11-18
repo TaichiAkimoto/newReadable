4d5fa23b512e5fb8887b234dfa174d89714609cc
TaichiAkimoto <italyitalienitalia@gmail.com>
2017-11-18 13:18:27 +0900

    Add redux-persist v4, but not seem to work properly

--- src/index.js
+++ src/index.js
@@ -3,25 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import {
   createStore,
   applyMiddleware,
   compose,
 } from 'redux'
 import reducer from './reducers'
-import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
+import { persistStore, autoRehydrate } from 'redux-persist'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(logger, thunk)
-  )
-)
+let store = compose(
+  applyMiddleware(logger, thunk),
+  autoRehydrate()
+)(createStore)(reducer);
+persistStore(store, {storage: localStorage})
+
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
